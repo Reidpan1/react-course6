d35a4fd9acd41a5b451f8ef80d74320a262ba581
Reidpan1 <129805612+Reidpan1@users.noreply.github.com>
2023-05-30 14:43:16 -0500

    static props for eventid

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,8 +1,28 @@
-export async function getAllEvents(){
-  const response = await fetch('https://nextjs-course-701f2-default-rtdb.firebaseio.com/events.json');
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-701f2-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+return events;
+
 }
 
 
 export async function getFeaturedEvents() {
-    return DUMMY_EVENTS.filter((event) => event.isFeatured);
-  }
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function HomePage() {
+function HomePage(props) {
   const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;

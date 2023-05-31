28c3b1402dec54775f137bed25ba64ef27ec24fc
Reidpan1 <129805612+Reidpan1@users.noreply.github.com>
2023-05-31 08:49:58 -0500

    final and working

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -9,13 +9,14 @@ import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage(props) {
- const [loadedEvents, setLoadedEvents] =  useState();
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
 
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    "https://nextjs-course-701f2-default-rtdb.firebaseio.com/events.json"
+    "https://nextjs-course-701f2-default-rtdb.firebaseio.com/events.json",
+    (url) => fetch(url).then(res => res.json())
   );
 
   useEffect(() => {
@@ -28,7 +29,7 @@ function FilteredEventsPage(props) {
           ...data[key],
         });
       }
-    setLoadedEvents(events);
+      setLoadedEvents(events);
     }
   }, [data]);
 
@@ -42,12 +43,15 @@ function FilteredEventsPage(props) {
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
 
-  const filteredEvents = loadedEvents.filter((event) => {
-    const eventDate = new Date(event.date);
-    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
-  });
-
-  if (props.hasError) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -60,7 +64,13 @@ function FilteredEventsPage(props) {
     );
   }
 
-  const filteredEvents = props.events;
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -75,7 +85,7 @@ function FilteredEventsPage(props) {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <Fragment>
@@ -85,49 +95,40 @@ function FilteredEventsPage(props) {
   );
 }
 
-export async function getServerSideProps(context) {
-  const { params } = context;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
 
-  const filterData = params.slug;
+//   const filterData = params.slug;
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
-    return {
-      props: { hasError: true },
+//   return {
+//     props: { hasError: true },
 
-      // notFound: true,
-      // redirect: {
-      //   destination: '/error'
-      // }
-    };
-  }
+// notFound: true,
+// redirect: {
+//   destination: '/error'
+//     // }
+//   };
+// }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+// const filteredEvents = await getFilteredEvents({
+//   year: numYear,
+//   month: numMonth,
+// });
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  };
-}
+// return {
+//   props: {
+//     events: filteredEvents,
+//     date: {
+//       year: numYear,
+//       month: numMonth,
+//     },
+//   },
+// };
 
 export default FilteredEventsPage;
